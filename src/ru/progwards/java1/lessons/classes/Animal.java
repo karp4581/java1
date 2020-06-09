2f0e8681756982ec841141acafce9bac2902bcd4
Karpova <natasha978@mail.ru>
2020-06-09 22:02:25 +0500

    reformat code

--- src/ru/progwards/java1/lessons/classes/Animal.java
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -12,67 +12,34 @@ public class Animal {
     enum AnimalKind {
         ANIMAL, COW, HAMSTER, DUCK,}
     enum FoodKind {UNKNOWN, HAY, CORN}
-    AnimalKind animal1 = AnimalKind.ANIMAL;
-    FoodKind food1 = FoodKind.UNKNOWN;
-    AnimalKind animal2 = AnimalKind.COW;
-    FoodKind food2 = FoodKind.HAY;
-    AnimalKind animal3 = AnimalKind.HAMSTER;
-    FoodKind food3 = FoodKind.CORN;
-    AnimalKind animal4 = AnimalKind.DUCK;
-    FoodKind food4 = FoodKind.CORN;
 
     public Animal(double weight) {
         this.weight=weight;}
 
-        public AnimalKind getKind () {return animal1;}
-        public FoodKind getFoodKind() {return food1;}
+        public AnimalKind getKind () {return AnimalKind.ANIMAL;}
+        public FoodKind getFoodKind() {return FoodKind.UNKNOWN;}
     public String toStringFull(){
-        return "I am" + "," + getKind() + " "  + "eat" + " " + getFoodKind() + " " + calculateFoodWeight();
+        return "I am" + "," + " " + getKind() + " "  + "eat" + " " + getFoodKind() + " " + calculateFoodWeight();
     }
 
     @Override
     public String toString() {
-        return "I am" + "," + getKind()   + "eat" + getFoodKind();
+        return "I am" + "," + " " + getKind()  + " " + "eat" + " " + getFoodKind();
     }
 
-   public class Cow extends Animal {
-       public Cow(double weight) {
-           super(weight);}
-
-       @Override
-       public AnimalKind getKind () {return animal2;}
-       @Override
-       public FoodKind getFoodKind() {return food2;}
-       @Override
-       public double getFoodCoeff() {return 0.05;}
-
-   }
-
-    public class Hamster extends Animal {
-        public Hamster(double weight) {
-            super(weight);
-        }
-        @Override
-        public AnimalKind getKind () {return animal3;}
-        @Override
-        public FoodKind getFoodKind() {return food3;}
-        @Override
-        public double getFoodCoeff() {return 0.03;}
-          }
-    public class Duck extends Animal {
-        public Duck(double weight) {
-            super(weight);
-        }
-        @Override
-        public AnimalKind getKind () {return animal4;}
-        @Override
-        public FoodKind getFoodKind() {return food4;}
-        @Override
-        public double getFoodCoeff() {return 0.04;}
-    }
-    public static void printInfo (Animal animal) {
+        public static void printInfo (Animal animal) {
         System.out.println(animal);
         System.out.println(animal.toStringFull());
 
+    }
+    public static void main(String[] args) {
+        Animal animal = new Animal(50.0);
+        Cow cow = new Cow(100.0);
+        Hamster hamster = new Hamster(1.0);
+        Duck duck = new Duck(3.1);
+        printInfo(cow);
+        printInfo(hamster);
+        printInfo(duck);
+        printInfo(animal);
     }}
 
